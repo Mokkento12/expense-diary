8d6a87cfb3a3ffe7b0200b6fd4fdbab0b28c7a24
roman alekseev <roma.alekseev.90@bk.ru>
2025-01-17 21:37:20 +0300

    Логика добавления на страницу

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const addExpenseBtn = document.getElementById("add-expense-btn");
 const amountInput = document.getElementById("amount");
 const categoryInput = document.getElementById("category");
+const expenseList = document.getElementById("expense-list");
 
 addExpenseBtn.addEventListener("click", () => {
   const amount = parseFloat(amountInput.value);
   const category = categoryInput.value.trim();
 
-  console.log(`Сумма: ${amount}, Категория: ${category}`);
+  // Проверка, что сумма не меньше 0
+
+  if (isNaN(amount) || amount <= 0) {
+    alert("Введите корректную сумму.");
+
+    return;
+  }
+
+  // Создаем новый элемент списка
+  const expenseItem = document.createElement("li");
+  expenseItem.textContent = `${category}: ${amount.toFixed(2)} ₽`;
+
+  // Добавляем элемент в список
+
+  expenseList.appendChild(expenseItem);
+
+  // Очищаем поля ввода
+
+  amountInput.value = "";
+  categoryInput.value = "";
 });
